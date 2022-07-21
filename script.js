a2637d11f733a30ecd9566e4fefe41318c81ed64
Bel-Olga <dovbnyaolga@gmail.com>
2022-07-21 17:01:24 +0300

    index.html

--- script.js
+++ script.js
@@ -1,61 +1,97 @@
-const choices = ['rock', 'paper', 'scissors'];
+
+const buttons = document.querySelectorAll('.btn');
+const restartButton = document.getElementById('restart');
+let resultDiv = document.getElementById('result');
+let userScoreDiv = document.querySelector('.user-score');
+let compScoreDiv = document.querySelector('.comp-score');
+
+
 
 let userScore = 0;
 let computerScore = 0;
 
-function computerPlay() {
-    const random = Math.floor(Math.random() * choices.length);
-    return choices[random];
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    playRound(button.value);
+  });
+});
     
+// Computer choice function
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];  
+}
+
+//Disable buttons
+function disableButtons() {
+  buttons.forEach(button => {
+    button.disabled = true;
+  })
 }
 
 
-function playRound(playerSelection, compSelection) {
-     // rock
+
+// Game
+function playRound(playerSelection) {
+  let compSelection = computerPlay(); 
+  let result = '';
+    
   if (playerSelection === compSelection) {
-      return "It's a tie!"
-  } else if (playerSelection === 'rock' && compSelection === 'paper') {
-      computerScore ++;
-      return "You lose! Paper beats rock"
-  } else if (playerSelection === 'rock' && compSelection === 'scissors') {
-      userScore ++;
-      return "You win! Rock beats scissors"
-  } //paper
-  else if (playerSelection === 'paper' && compSelection === 'rock') {
-    userScore ++;
-    return "You win! Paper beats rock"
-  } else if (playerSelection === 'paper' && compSelection === 'scissors') {
-    computerScore ++;
-    return "You lose! Scissors beats paper"
-  } // scissors
-  else if (playerSelection === 'scissors' && compSelection === 'rock') {
-    computerScore ++;
-    return "You lose! Rock beats scissors"
-  } else if (playerSelection === 'scissors' && compSelection === 'paper') {
-    userScore ++;
-      return "You win! Scissors beats paper"
+    result = "It's a tie!"
+  } else if ((playerSelection == 'rock' && compSelection == 'scissors') ||  
+            (playerSelection == 'paper' && compSelection == 'rock') ||
+            (playerSelection == 'scissors' && compSelection == 'paper')) {
+  userScore ++;
+  
+  result = "You win! " + playerSelection + " beats " + compSelection + '. ';
+            } else {
+  computerScore ++;
+  result = "You lose! " + compSelection + " beats " + playerSelection + '. ';
+            }
+
+  
+  if (userScore === 5) {
+    disableButtons();
+    result += "You win the game! Press restart to play again."
+  } else if (computerScore === 5) {
+    disableButtons();
+    result += "You lose the game! Press restart to play again."
   }
 
-  console.log('user ' + userScore + '; comp ' + computerScore);
-}
+console.log('user: '+ playerSelection + ' / comp: ' + compSelection);
+
+// Add result to HTML
+resultDiv.textContent = result;
+userScoreDiv.textContent = `${userScore}`;
+compScoreDiv.textContent = `${computerScore}`;
+
+//Restart game
+restartButton.addEventListener('click', () => {
+  buttons.forEach(button => {
+    button.disabled = false;
+  });
+  userScore = 0;
+  computerScore = 0;
+
+  userScoreDiv.textContent = '';
+  compScoreDiv.textContent = '';
+  resultDiv.textContent = '';
+  
+});
+};
+
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Please choose rock, paper or scissors: ").toLowerCase();
-        //Check is input correct
-        
-
-        let compSelection = computerPlay(); 
-       console.log(playRound(playerSelection, compSelection)); 
-        
-    }
     if(computerScore > userScore) {
         return "You loose the game!"
     } else if (userScore > computerScore) {
         return "You win the game!"} else {
             return "It`s a tie!"
         }
-}
-
-
-console.log(game());
+}        
+    }
+    
