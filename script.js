cf055a88ef21cdd51e20b7192952002f4e67a9bf
Bel-Olga <dovbnyaolga@gmail.com>
2022-09-12 11:36:48 +0300

    Code refactoring

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-
 const buttons = document.querySelectorAll('.btn-choices');
 const restartButton = document.getElementById('restart');
-let resultDiv = document.getElementById('result');
-let scoreDiv = document.querySelector('.score-board');
 const userChoice = document.getElementById('user-element');
 const compChoice = document.getElementById('comp-element');
 const title = document.querySelector('.title');
+const resultDiv = document.getElementById('result');
+const scoreDiv = document.querySelector('.score-board');
 
+const choices = ['rock', 'paper', 'scissors'];
 
 let userScore = 0;
 let computerScore = 0;
 
+//Add choices icons
+const handleChoicesIcons = (playerSelection, compSelection) => {
+  userChoice.classList = `fa-solid fa-hand-${playerSelection} active`;
+  compChoice.classList = `fa-solid fa-hand-${compSelection} active`;
+};
 
+const renderGameResult = (playerSelection, compSelection, result) => {
+  resultDiv.innerHTML = result;
+  scoreDiv.textContent = `${computerScore} : ${userScore}`;
+  handleChoicesIcons(playerSelection, compSelection);
+}
 
-buttons.forEach(button => {
-  button.addEventListener('click', function () {
-    playRound(button.value);
-  });
-});
-    
 // Computer choice function
 function computerPlay() {
-    const choices = ['rock', 'paper', 'scissors'];
-    const random = Math.floor(Math.random() * choices.length);
-    return choices[random];  
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
 }
 
 //Disable buttons
@@ -33,50 +36,13 @@ function disableButtons() {
   })
 }
 
+const isPlayerWin = (playerSelection, compSelection) => {
+  return (playerSelection == 'rock' && compSelection == 'scissors') ||
+    (playerSelection == 'paper' && compSelection == 'rock') ||
+    (playerSelection == 'scissors' && compSelection == 'paper')
+}
 
-
-// Game
-function playRound(playerSelection) {
-  let compSelection = computerPlay(); 
-  let result = '';
-    
-  if (playerSelection === compSelection) {
-    result = "It's a tie!"
-  } else if ((playerSelection == 'rock' && compSelection == 'scissors') ||  
-            (playerSelection == 'paper' && compSelection == 'rock') ||
-            (playerSelection == 'scissors' && compSelection == 'paper')) {
-  userScore ++;
-  
-  result = "You win: " + playerSelection + " beats " + compSelection + '. ';
-            } else {
-  computerScore ++;
-  result = "You lose: " + compSelection + " beats " + playerSelection + '. ';
-            }
-
-  
-  if (userScore === 5) {
-    disableButtons();
-    restartButton.classList.remove('visually-hidden');  
-    result += `You win the game!<br> Press restart to play again.`;
-    title.style.color = "rgb(27, 189, 27)";
-  } else if (computerScore === 5) {
-    disableButtons();
-    restartButton.classList.remove('visually-hidden');  
-    result += `You lose the game!<br> Press restart to play again.`;
-    title.style.color = "red";
-  }
-
-console.log('user: '+ playerSelection + ' / comp: ' + compSelection);
-
-// Add result and score to HTML
-resultDiv.innerHTML = result;
-scoreDiv.textContent = `${computerScore} : ${userScore}`;
-choicesIcons(playerSelection, compSelection);
-
-
-
-//Restart game
-restartButton.addEventListener('click', () => {
+const handleRestartGame = () => {
   buttons.forEach(button => {
     button.disabled = false;
   });
@@ -86,35 +52,49 @@ restartButton.addEventListener('click', () => {
   scoreDiv.textContent = '';
   resultDiv.textContent = '';
 
-  userChoice.classList  = 'fa-solid ';
+  userChoice.classList = 'fa-solid ';
   compChoice.classList = 'fa-solid ';
-  restartButton.classList.add('visually-hidden');  
+  restartButton.classList.add('visually-hidden');
 
   title.style.color = 'white';
-});
+}
 
-//Add choices icons
-function choicesIcons(playerSelection, compSelection) {
+const handleEndGameResult = (userScore, computerScore) => {
+  if (userScore === 5) {
+    restartButton.classList.remove('visually-hidden');
+    result += `You win the game!<br> Press restart to play again.`;
+    title.style.color = "rgb(27, 189, 27)";
+  } else if (computerScore === 5) {
+    restartButton.classList.remove('visually-hidden');
+    result += `You lose the game!<br> Press restart to play again.`;
+    title.style.color = "red";
+  }
+
+  disableButtons();
+}
+
+// Game
+function playRound(playerSelection) {
+  let compSelection = computerPlay();
+  let result = '';
+
+  if (playerSelection === compSelection) {
+    result = `It's a tie!`;
+  } else if (isPlayerWin(playerSelection, compSelection)) {
+    userScore++;
+    result = `You win: ${playerSelection} beats ${compSelection}.`;
+  } else {
+    computerScore++;
+    result = `You lose: ${compSelection} beats ${playerSelection}`;
+  }
+
+  if (userScore === 5 || computerScore === 5) handleEndGameResult(userScore, computerScore);
+
+  console.log('user: ' + playerSelection + ' / comp: ' + compSelection);
   
 
-   userChoice.classList = `fa-solid fa-hand-${playerSelection} active`;
-   compChoice.classList = `fa-solid fa-hand-${compSelection} active`;
-};
+  renderGameResult(playerSelection, compSelection, result);
 };
 
-
-
-
-
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//     if(computerScore > userScore) {
-//         return "You loose the game!"
-//     } else if (userScore > computerScore) {
-//         return "You win the game!"} else {
-//             return "It`s a tie!"
-//         }
-// } 
-    
-//     }
-    
+buttons.forEach(button => button.addEventListener('click', () => playRound(button.value)));
+restartButton.addEventListener('click', handleRestartGame);
